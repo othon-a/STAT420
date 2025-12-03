55e081d51fbc5c57020826b78a9e2665e1102bf2
othon-a <156286553+othon-a@users.noreply.github.com>
2025-12-02 23:54:26 -0600

    try interactions and quadtratic transformations

--- Othon_work/models.R
+++ Othon_work/models.R
@@ -1,12 +1,13 @@
 library(readr)
+
 df = read_csv("cleaned_yield_with_gdp_cat.csv")
 df_potatoes = subset(df, crop == "Potatoes")
+df_potatoes$rain_mm_cent = df_potatoes$rain_mm - mean(df_potatoes$rain_mm)
+df_potatoes$temp_c_cent = df_potatoes$temp_c - mean(df_potatoes$temp_c)
+df_potatoes$year_cent = df_potatoes$year - mean(df_potatoes$year)
 
-subregions_add = lm(yield_hg_ha ~ year + rain_mm + pesticides_t + temp_c + gdp_tier + subregion, data = df_potatoes)
-summary(lin_model)
+add_model = lm(yield_hg_ha ~ rain_mm + temp_c + pesticides_t + year + gdp_tier + , , data = df_potatoes)
+summary(add_model)
 
-gdp_int = lm(yield_hg_ha ~ year + rain_mm + pesticides_t + temp_c + gdp_tier + gdp_tier:year + gdp_tier:rain_mm + gdp_tier:pesticides_t + gdp_tier:temp_c, data = df_potatoes)
-summary(gdp_int)
-
-lin_int = lm(yield_hg_ha ~ year * rain_mm * pesticides_t * temp_c * gdp_tier, data = df_potatoes)
-summary(lin_int)
+model2 = lm(yield_hg_ha ~ rain_mm_cent * temp_c_cent + I(rain_mm_cent ^ 2) + I(temp_c_cent ^ 2) + year_cent + pesticides_t + gdp_tier + subregion, data = df_potatoes)
+summary(model2)
