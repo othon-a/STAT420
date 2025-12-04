448bee79eb3cb6d20adf88ebee610ae3d57ab185
othon-a <156286553+othon-a@users.noreply.github.com>
2025-12-03 22:31:02 -0600

    Try models and test diagnostics with and without subregions predictor

--- Othon_work/models.R
+++ Othon_work/models.R
@@ -1,13 +1,47 @@
 library(readr)
+library(lmtest)
+library(car)
 
+# subset potatoes and center predictors
 df = read_csv("cleaned_yield_with_gdp_cat.csv")
 df_potatoes = subset(df, crop == "Potatoes")
 df_potatoes$rain_mm_cent = df_potatoes$rain_mm - mean(df_potatoes$rain_mm)
 df_potatoes$temp_c_cent = df_potatoes$temp_c - mean(df_potatoes$temp_c)
 df_potatoes$year_cent = df_potatoes$year - mean(df_potatoes$year)
 
-add_model = lm(yield_hg_ha ~ rain_mm + temp_c + pesticides_t + year + gdp_tier + , , data = df_potatoes)
+# additive model
+add_model = lm(yield_hg_ha ~ rain_mm + temp_c + pesticides_t + year + gdp_tier + subregion, , data = df_potatoes)
 summary(add_model)
 
-model2 = lm(yield_hg_ha ~ rain_mm_cent * temp_c_cent + I(rain_mm_cent ^ 2) + I(temp_c_cent ^ 2) + year_cent + pesticides_t + gdp_tier + subregion, data = df_potatoes)
-summary(model2)
+# test polynomial models of different degrees
+poly_model = lm(yield_hg_ha ~ poly(rain_mm_cent, 2) + poly(temp_c_cent, 2) + year_cent + pesticides_t + gdp_tier + subregion, data = df_potatoes)
+summary(poly_model)
+
+# polynomial model diagnostics
+plot(resid(poly_model) ~ fitted(poly_model))
+abline(h = 0)
+bptest(poly_model)
+qqnorm(resid(poly_model))
+qqline(resid(poly_model))
+shapiro.test(resid(poly_model))
+
+# the quadratic model featuring subregions attains a high r-squared and somewhat reasonable normality
+quad_model = lm(yield_hg_ha ~ rain_mm_cent + temp_c_cent + I(rain_mm_cent ^ 2) + I(temp_c_cent ^ 2) + year_cent + pesticides_t + gdp_tier + subregion, data = df_potatoes)
+summary(quad_model)
+
+# however, we see issues with collinearity
+# still, it seems to be a decent model for prediction
+vif(quad_model)
+
+# we can remove collinarity issues by removing subregions from the model
+# however, r-squared and other diagnostics suffer
+nosubreg_quad = lm(yield_hg_ha ~ rain_mm_cent * temp_c_cent + I(rain_mm_cent ^ 2) + I(temp_c_cent ^ 2) + year_cent + pesticides_t + gdp_tier, data = df_potatoes)
+summary(nosubreg_quad)
+vif(nosubreg_quad)
+
+plot(resid(poly_model) ~ fitted(poly_model))
+abline(h = 0)
+bptest(poly_model)
+qqnorm(resid(poly_model))
+qqline(resid(poly_model))
+shapiro.test(resid(poly_model))
